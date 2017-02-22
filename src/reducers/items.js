8339b1cf9e161aff2490fc2ca3c56c3e28893d30
hayden7913 <hayden321@gmail.com>
2017-02-22 10:52:58 -0800

    webpack -loader

---
+++ src/reducers/items.js
@@ -0,0 +1,36 @@
+/*import * as actions from '../actions/index';
+
+const initialState = { 
+  pos: [0, 0],
+  cardHasRendered: false
+};
+
+export function moveDown(state = initialState, action){
+  if (action.type === actions.MOVE_DOWN) {
+    return { 
+      ...state,
+      pos: action.pos
+    };
+}
+
+export function fetchPosition(state = initialState, action){
+  if (action.type === actions.FETCH_POSITION_SUCCESS) {
+    return { 
+      ...state,
+      pos: action.pos.position
+    };
+  }
+
+  return state;
+}
+    
+export function cardHasRendered(state = initialState, action){
+  if (action.type === actions.FIRST_RENDER) {
+    return { 
+      ...state, 
+      cardHasRendered: action.bool 
+    };
+  }
+  
+  return state;
+}*/
